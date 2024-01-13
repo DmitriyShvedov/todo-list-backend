d17cea6d845fe2e383ad5c618ca9497633067fb5
DmitriyShvedov <89867550+DmitriyShvedov@users.noreply.github.com>
2024-01-13 20:24:05 +0200

    new project

--- web_server.py
+++ web_server.py
@@ -5,6 +5,7 @@ FOLDER = '/Users/dm.shvedov/Desktop'
 
 app = Flask(__name__)
 
+
 @app.route("/api/entries/")
 def get_entries():
     my_list = []
@@ -17,17 +18,13 @@ def get_entries():
 
 @app.route('/api/save_entries/', methods=["POST"])
 def save_entries():
-        data = request.get_json()
-
-        entry_manager = EntryManager(FOLDER)
-
-        for entry_data in data:
-            entry = Entry.from_json(entry_data)
-            entry_manager.entries.append(entry)
-
-        entry_manager.save()
-
-        return {'status': 'success'}
+    data = request.get_json()
+    entry_manager = EntryManager(FOLDER)
+    for entry_data in data:
+        entry = Entry.from_json(entry_data)
+        entry_manager.entries.append(entry)
+    entry_manager.save()
+    return {'status': 'success'}
 
 
 @app.after_request
@@ -36,6 +33,7 @@ def after_request(response):
     response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
     response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
     return response
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000, debug=False)
